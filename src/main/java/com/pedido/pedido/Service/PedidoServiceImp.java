a8f73e2c98440dade5acb920709aa42908821428
Sebastian Valdivia <sebastian.valdivia97@gmail.com>
2025-03-30 20:48:18 -0300

    Se agregan datos dummys

--- src/main/java/com/pedido/pedido/Service/PedidoServiceImp.java
+++ src/main/java/com/pedido/pedido/Service/PedidoServiceImp.java
@@ -14,10 +14,10 @@ public class PedidoServiceImp implements PedidoService  {
 
 	public PedidoServiceImp() {
 		pedidos = new ArrayList<>();
-		pedidos.add(new Pedido(1, "Pedido1", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 100));
-		pedidos.add(new Pedido(2, "Pedido2", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 200));
-		pedidos.add(new Pedido(3, "Pedido3", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 300));
-		pedidos.add(new Pedido(4, "Pedido4", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 400));
+		pedidos.add(new Pedido(1, "Sebastian Valdivia", "Duocuc", "asd #123", "Chile", "Santiago", "1235546", "+56912345678", "+12345698778", "asd@asd.cl", "31-03-2025", "01-04-2025", "01-04-2025", "Entregado", "5325234523", "Pedido fragil", 20.5, 10.0, 20));
+		pedidos.add(new Pedido(2, "Juan Perez", "EmpresaX", "calle #456", "Chile", "Valparaiso", "654321", "+56987654321", "+98765432100", "juan@empresa.com", "01-04-2025", null, "02-04-2025", "En camino", "1234567890", "Pedido urgente", 15.0, 8.0, 25));
+		pedidos.add(new Pedido(3, "Maria Lopez", "EmpresaY", "avenida #789", "Chile", "Concepcion", "789123", "+56911223344", "+11223344556", "maria@empresa.com", "02-04-2025", null, "03-04-2025", "Pendiente", "0987654321", "Pedido estándar", 10.0, 5.0, 30));
+		pedidos.add(new Pedido(4, "Carlos Gomez", "EmpresaZ", "pasaje #321", "Chile", "Antofagasta", "321987", "+56955667788", "+55667788990", "carlos@empresa.com", "03-04-2025", null, "04-04-2025", "Cancelado", "5678901234", "Pedido delicado", 25.0, 12.0, 35));
 	}
 
 	@Override
