77db1b017970250cebafe755633f478a0ba6dcce
valdo0 <sebastian.valdivia97gmail.com>
2025-04-20 23:15:59 -0400

    Agrega métodos para crear, actualizar, eliminar y modificar parcialmente pedidos en la interfaz PedidoService

--- src/main/java/com/pedido/pedido/Service/PedidoService.java
+++ src/main/java/com/pedido/pedido/Service/PedidoService.java
@@ -3,8 +3,14 @@ package com.pedido.pedido.Service;
 import java.util.List;
 
 import com.pedido.pedido.Model.Pedido;
+import com.pedido.pedido.api.request.PedidoCreateRequest;
+import com.pedido.pedido.api.request.PedidoPatchRequest;
 
 public interface PedidoService {
     List<Pedido> getAllPedidos();
-    Pedido getPedidoById(int id);
+    Pedido getPedidoById(Long id);
+    Pedido createPedido(PedidoCreateRequest pedido);
+    Pedido updatePedido(Long id, PedidoCreateRequest pedido);
+    boolean deletePedido(Long id);
+    Pedido patchPedido(Long id, PedidoPatchRequest pedido);
 }
